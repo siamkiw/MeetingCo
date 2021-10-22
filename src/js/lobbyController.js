cd548364a1da4ec888e6151ce07dc3cbb8f075db
siam.kiw <siam.kiw@gmail.com>
2021-10-22 11:45:43 +0700

    update lobbyPage

--- src/js/lobbyController.js
+++ src/js/lobbyController.js
@@ -3,3 +3,15 @@ var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
   })
 
 myModal.show()
+
+let userContainer = document.querySelector("#user-container")
+let registerButton = document.querySelector("#register-button")
+let isDisplayUser = false
+registerButton.addEventListener('click', () => {
+    if(!isDisplayUser){
+    userContainer.style.display = "block"
+    }else{
+    userContainer.style.display = "none"
+    }
+    isDisplayUser = !isDisplayUser
+})
